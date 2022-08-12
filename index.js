3b72fac932796e76183827d0325cda9319c98fcc
sean-code <john.nganga@student.moringaschool.com>
2022-08-12 15:09:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+// main.remove();
+
+// const newHeader = document.createElement('h1');
+
+// newHeader.innerText = 'h1#victory';
+
+// const main = document.getElementById('victory')
+
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "John Nganga is the champion";
+
+newHeader.id = "victory";
+document.body.append(newHeader);
+
+console.log(newHeader);
+
